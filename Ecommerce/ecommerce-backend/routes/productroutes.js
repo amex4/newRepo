df653c3b5d5050ef52f1d88a89fa882ea50700a4
amex4 <74108330+amex4@users.noreply.github.com>
2024-08-08 20:43:52 +0530

    Update productroutes.js

--- Ecommerce/ecommerce-backend/routes/productroutes.js
+++ Ecommerce/ecommerce-backend/routes/productroutes.js
@@ -7,7 +7,7 @@ const authorizeRole = require('../middleware/authrolemiddleware');
 router.post('/', authenticateJWT, authorizeRole('superadmin'), productController.addProduct);
 router.get('/', authenticateJWT, productController.getProducts);
 router.get('/:id',authenticateJWT,productController.getProductById);
-router.delete('/delete/:id',authenticateJWT, authorizeRole('superadmin'), productController.deleteProduct);
+router.post('/delete/:id',authenticateJWT, authorizeRole('superadmin'), productController.deleteProduct);
 router.put('/update/:id',authenticateJWT,authorizeRole('superadmin'), productController.updateProduct);
 
 module.exports = router;    
